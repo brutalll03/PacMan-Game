a6e6b6495a1903469da50552afd650ec0bf6c5fa
wanesa <wbrutel@edu.cdv.pl>
2024-04-17 19:18:55 +0200

    Movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,12 +1,13 @@
-
 using UnityEngine;
-[RequireComponent (typeof(Rigidbody2D))]
+
+[RequireComponent(typeof(Rigidbody2D))]
 public class Movement : MonoBehaviour
 {
-    public float speed = 8.0f;
-    public float speedMultiplier = 1.0f;
+    public float speed = 8f;
+    public float speedMultiplier = 1f;
     public Vector2 initialDirection;
     public LayerMask obstacleLayer;
+
     public new Rigidbody2D rigidbody { get; private set; }
     public Vector2 direction { get; private set; }
     public Vector2 nextDirection { get; private set; }
@@ -14,8 +15,8 @@ public class Movement : MonoBehaviour
 
     private void Awake()
     {
-        this.rigidbody = GetComponent<Rigidbody2D>();
-        this.startingPosition = this.transform.position;
+        rigidbody = GetComponent<Rigidbody2D>();
+        startingPosition = transform.position;
     }
 
     private void Start()
@@ -23,38 +24,48 @@ public class Movement : MonoBehaviour
         ResetState();
     }
 
-    private void ResetState()
+    public void ResetState()
     {
-        this.speedMultiplier = 1.0f;
-        this.direction = this.initialDirection;
-        this.nextDirection = Vector2.zero;
-        this.transform.position = this.startingPosition;
-        this.rigidbody.isKinematic = false;
-        this.enabled = true;
+        speedMultiplier = 1f;
+        direction = initialDirection;
+        nextDirection = Vector2.zero;
+        transform.position = startingPosition;
+        rigidbody.isKinematic = false;
+        enabled = true;
+    }
+
+    private void Update()
+    {
+        if (nextDirection != Vector2.zero)
+        {
+            SetDirection(nextDirection);
+        }
     }
 
     private void FixedUpdate()
     {
-        Vector2 position = this.rigidbody.position;
-        Vector2 translation = this.direction * this.speed * this.speedMultiplier * Time.fixedDeltaTime;
-        this.rigidbody.MovePosition(position + translation);
+        Vector2 position = rigidbody.position;
+        Vector2 translation = direction * speed * speedMultiplier * Time.fixedDeltaTime;
+
+        rigidbody.MovePosition(position + translation);
     }
 
-    public void SetDirection(Vector2 direction)
+    public void SetDirection(Vector2 direction, bool forced = false)
     {
-
+        if (forced || !Occupied(direction))
+        {
+            this.direction = direction;
+            nextDirection = Vector2.zero;
+        }
+        else
+        {
+            nextDirection = direction;
+        }
     }
 
-    /*  public bool Occupied(Vector2 direction)
-      {
-          RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * 0.75f, 0.0f, direction, 1.5f, this.);
-      }*/
-
-    public void OnMove(string xAxis, string yAxis)
+    public bool Occupied(Vector2 direction)
     {
-        float x = Input.GetAxis(xAxis);
-        float y = Input.GetAxis(yAxis);
-
-        this.rigidbody.velocity = new Vector2(x, y) * speed;
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, direction, 1.5f, obstacleLayer);
+        return hit.collider != null;
     }
 }
