a3df2016e396f8a772448024ef592b328cc0cd1c
wanesa <wbrutel@edu.cdv.pl>
2024-04-24 18:07:52 +0200

    Assets

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -41,40 +41,44 @@ public class Movement : MonoBehaviour
 
     private void Update()
     {
-        RaycastHit2D upHit = Physics2D.Raycast(transform.position, Vector2.up, 0.8f, obstacleLayer);
+        RaycastHit2D upHit = Physics2D.BoxCast(transform.position, Vector2.one * .1f, 0, Vector2.up, .6f, obstacleLayer);
         upWall = upHit.collider != null;
 
-        RaycastHit2D downHit = Physics2D.Raycast(transform.position, Vector2.down, 0.8f, obstacleLayer);
-        downWall = downHit.collider != null;
-
-        RaycastHit2D leftHit = Physics2D.Raycast(transform.position, Vector2.left, 0.8f, obstacleLayer);
+        RaycastHit2D downHit = Physics2D.BoxCast(transform.position, Vector2.one * .1f, 0, Vector2.down, .6f, obstacleLayer);
+        downWall = downHit.collider != null;                                         
+                                                                                     
+        RaycastHit2D leftHit = Physics2D.BoxCast(transform.position, Vector2.one * .1f, 0, Vector2.left, .6f, obstacleLayer);
         leftWall = leftHit.collider != null;
 
-        RaycastHit2D rightHit = Physics2D.Raycast(transform.position, Vector2.right, 0.8f, obstacleLayer);
+        RaycastHit2D rightHit = Physics2D.BoxCast(transform.position, Vector2.one * .1f, 0, Vector2.right, .6f, obstacleLayer);
         rightWall = rightHit.collider != null;
 
         if(rigidbody.velocity.normalized ==  Vector2.right && rightWall)
         {
             rigidbody.velocity = Vector2.zero;
             rigidbody.constraints = RigidbodyConstraints2D.None;
+            rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
         }
 
         if(rigidbody.velocity.normalized == Vector2.left && leftWall)
         {
             rigidbody.velocity = Vector2.zero;
             rigidbody.constraints = RigidbodyConstraints2D.None;
+            rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
         }
 
         if (rigidbody.velocity.normalized == Vector2.up && upWall)
         {
             rigidbody.velocity = Vector2.zero;
             rigidbody.constraints = RigidbodyConstraints2D.None;
+            rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
         }
 
         if (rigidbody.velocity.normalized == Vector2.down && downWall)
         {
             rigidbody.velocity = Vector2.zero;
             rigidbody.constraints = RigidbodyConstraints2D.None;
+            rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
         }
 
 
@@ -82,22 +86,26 @@ public class Movement : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.LeftArrow)) 
         {
             rigidbody.velocity = Vector2.left * speed;
-            rigidbody.constraints = RigidbodyConstraints2D.FreezePositionY;
+            rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
+            rigidbody.rotation = 180;
         }
         else if (Input.GetKeyUp(KeyCode.RightArrow))
         {
             rigidbody.velocity = Vector2.right * speed;
-            rigidbody.constraints = RigidbodyConstraints2D.FreezePositionY;
+            rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
+            rigidbody.rotation = 0;
         }
         else if (Input.GetKeyUp(KeyCode.UpArrow))
         {
             rigidbody.velocity = Vector2.up * speed;
-            rigidbody.constraints = RigidbodyConstraints2D.FreezePositionX;
+            rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+            rigidbody.rotation = 90;
         }
         else if (Input.GetKeyUp(KeyCode.DownArrow))
         {
             rigidbody.velocity = Vector2.down * speed;
-            rigidbody.constraints = RigidbodyConstraints2D.FreezePositionX;
+            rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionX;
+            rigidbody.rotation = 270;
         }
     }
 }
